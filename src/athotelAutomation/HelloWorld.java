10010d998f501348797efd56cc2035176bf8a5e9
sagarraipurkar <er.sagarraipurkar@gmail.com>
2024-10-01 19:30:47 +0530

    New File added

---
+++ src/athotelAutomation/HelloWorld.java
@@ -0,0 +1,12 @@
+
+package athotelAutomation;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sagar Raipurkar");
+
+	}
+
+}
