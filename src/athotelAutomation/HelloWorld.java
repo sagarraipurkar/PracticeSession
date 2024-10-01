7e6231ccb59941fa5157c34342337f604c885a79
sagarraipurkar <er.sagarraipurkar@gmail.com>
2024-10-01 22:57:47 +0530

    we slove the error

--- src/athotelAutomation/HelloWorld.java
+++ src/athotelAutomation/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
-		
+
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
@@ -20,20 +20,17 @@ public class HelloWorld {
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
-		
+
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
 		System.out.println("Sagar Raipurkar");
-		
-		
 
 		System.out.println("Juhi Raipurkar");
 		System.out.println("Juhi Raipurkar");
 		System.out.println("Juhi Raipurkar");
 		System.out.println("Juhi Raipurkar");
-		
 
 		System.out.println("asha Raipurkar");
 		System.out.println("asha Raipurkar");
